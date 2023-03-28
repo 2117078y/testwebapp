dcd97084ce6dc32c1f149eae1f6bc1f92a95b4fa
2117078y <sub2117078y@gmail.com>
2023-03-28 19:46:42 +0900

    chatGPT搭載完了

--- flask/hello.py
+++ flask/hello.py
@@ -1,9 +1,13 @@
 #! /home/xs522872/miniconda3/envs/condapy310/bin/python3.10
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+import openai
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+# Set your API key here
+openai.api_key = "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"
 
 @app.route('/')
 def hello():
@@ -13,7 +17,15 @@ def hello():
 def display_text():
     if request.method == 'POST':
         text = request.form.get('text', '')
-        return f'You entered: {text}'
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "ChatGPTへの指示"},
+            {"role": "user", "content": f"{text}"}
+        ]   
+        )
+        chatgpt_output = response['choices'][0]['message']['content'].strip()
+        return render_template('output.html', input_text=text, chatgpt_output=chatgpt_output)
     else:
         return render_template('display.html')
 
